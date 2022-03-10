110a28dcb4bf57bcde631a1edabe2edc396d5e75
ainahazlin <ainahazlin1410@gmail.com>
2022-03-10 14:40:59 +0800

    Location Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class Location {
+	private String locate;
+
+	public String getLocate() {
+		return locate;
+	}
+
+	public void setLocate(String locate) {
+		this.locate = locate;
+	}
+	
+}
